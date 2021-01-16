ffc17406ff9dace4ec9e7c9b829b156f56594744
saddamyou <saddamyoussoufmoussa>
2021-01-17 02:32:54 +0800

    add two numbers

---
+++ addtwonumber.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+	
+	int x, y ; 
+	
+	int sum;
+	
+	cout <<"Enter two number of integer:" << endl;
+	
+	cin >> x >> y;
+
+	sum = x + y;
+	
+	cout <<" The total of the two number:"<< sum << endl;
+
+	return 0;
+	
+	
+}
